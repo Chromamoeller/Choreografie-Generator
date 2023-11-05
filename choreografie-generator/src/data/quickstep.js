9cc40bab63ffe58955d247256b7a6d516f744b39
Chromamoeller <der.chris.moeller@gmail.com>
2023-11-05 15:14:16 +0100

    add  figures to all dances

--- choreografie-generator/src/data/quickstep.js
+++ choreografie-generator/src/data/quickstep.js
@@ -1,6 +1,13 @@
 import { Dance } from "./dance";
 import { StandardFigure } from "./standardFigure";
 
+
+
+quaterTurnToRight = new StandardFigure("1 bis 4 Grundschritt", "D", "RF", "RF", false, false, "Facing DW", "Backing DC")
+progressiveChasse = new StandardFigure("5 bis 8 Grundschritt", "D", "RF", "RF", false, false, "Backing DC", "Facing DW")
+
 export let quickstep = new Dance("Quickstep", "50", "Standard", [
+    quaterTurnToRight,
+    progressiveChasse
 
 ])
--- choreografie-generator/src/data/tango.js
+++ choreografie-generator/src/data/tango.js
@@ -1,6 +1,12 @@
 import { Dance } from "./dance";
 import { StandardFigure } from "./standardFigure";
 
+walkingStepLF = new StandardFigure("Gehschritt mit dem linken Fuß", "D", "LF", "RF", false, false, "Facing DW", "Facing DW")
+walkingStepRF = new StandardFigure("Gehschritt mit dem rechten Fuß", "D", "RF", "LF", false, false, "Facing DW", "Facing DW")
+
+
 export let tango = new Dance("Tango", "30", "Standard", [
+    walkingStepLF,
+    walkingStepRF
 
 ])
